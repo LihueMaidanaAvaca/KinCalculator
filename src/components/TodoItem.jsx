2d909211b3c8afb19ee8134a88641c37a4f54706
LihueMaidanaAvaca <lihuevida@gmail.com>
2022-10-27 13:04:12 -0300

    functions.2

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import yearK from '../utils/yearK';
-import dayAndMonthK from '../utils/dayAndMonthK,js';
+import dayAndMonthK from '../utils/dayAndMonthK.js';
 
 export function TodoItem({ date }) {
   const { day : d, month : m, year : y }= date
 
     return (
     <li>
-        {yearK(y)}
+        {dayAndMonthK(d, m)}{yearK(y)}
     </li>
   )
 }
