88cd385ce07174b1657bb961e1aba8c859364143
LihueMaidanaAvaca <lihuevida@gmail.com>
2022-10-28 11:37:25 -0300

    deploy

--- src/utils/dayAndMonthK.js
+++ src/utils/dayAndMonthK.js
@@ -1,30 +1,48 @@
-export default function dayAndMonthK(day, month) {  
+export default function dayAndMonthK(day,month) {  
+  let tablaOne = 0;
   let d = parseInt(day)
   let m = parseInt(month)
-    if(month === 1) {
-      return d+54;
-    } else if (month === 2) {
-      return d+85;
-    } else if (month === 3) {
-      return d+113;
-    } else if (m === 4) {
-      return d+144;
-    } else if (month === 5) {
-      return d+174;
-    } else if (month === 6) {
-      return d+206;
-    } else if (month === 7) {
-      return d+236;
-    } else if (month === 8) {
-      return d+206;
-    } else if (month === 9) {
-      return d+37;
-    } else if (month === 10) {
-      return d+67;
-    } else if (month === 11) {
-      return d+98;
-    } else if (month === 12) {
-      return d+128;
-    } 
-      
-  }
+  
+  switch (m) {
+  case 1:
+    tablaOne = d+54;
+    break;
+  case 2:
+    tablaOne = d+85;
+    break;
+  case 3:
+    tablaOne = d+113;
+    break;
+  case 4:
+    tablaOne = d+144;
+    break;
+  case 5:
+    tablaOne = d+174;
+    break;
+  case 6:
+    tablaOne = d+206;
+    break;
+  case 7:
+    tablaOne = d+235; 
+    break;
+  case 8:
+    tablaOne = d+6;
+    break;
+  case 9:
+    tablaOne = d+37;
+    break;
+  case 10:
+    tablaOne = d+67;
+    break;   
+  case 11:
+    tablaOne = d+98;
+    break;
+  case 12:
+    tablaOne = d+128;
+    break;
+  default:
+    tablaOne = 'Lo lamentamos, por el momento no disponemos de ' + d + '.';
+}
+
+    return tablaOne
+}
