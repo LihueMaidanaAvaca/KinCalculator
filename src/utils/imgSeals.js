7deae901d43da11f93aea1224906398b9886f295
LihueMaidanaAvaca <lihuevida@gmail.com>
2022-11-01 21:31:18 -0300

    assets seals

--- src/utils/imgSeals.js
+++ src/utils/imgSeals.js
@@ -1,9 +1,13 @@
+import NuEtznab from '../assets/NuEtznab.png' ;
 import NuAhau from '../assets/NuAhau.png';
+
 
 export default function imgSeals(seal){
     switch(seal){
         case 0:
             return NuAhau;
+        case 18:
+            return NuEtznab;
         default:
             return 'nothing'    
 
