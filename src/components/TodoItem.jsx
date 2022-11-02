ab3ec8ebed389ccc946a5cd055deab84c83fd51f
LihueMaidanaAvaca <lihuevida@gmail.com>
2022-11-02 13:41:10 -0300

    20seals

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import yearK from '../utils/yearK';
 import dayAndMonthK from '../utils/dayAndMonthK.js';
 import toKin from '../utils/toKin';
+import style from './TodoItem.css'
 
 export function TodoItem({ date }) {
   const { day : d, month : m, year : y } = date
@@ -9,8 +10,13 @@ export function TodoItem({ date }) {
   let kin = toKin( yearK(y), dayAndMonthK(d,m) )
 
     return (
-    <><img src={kin.image} alt="sello"></img><li>
+    <>
+     <div className={style.img}>      
+      <img src={kin.image} alt="sello" />
+     </div>
+     <li>
         {[kin.Umbral]}
-      </li></>
+     </li>
+    </>
   )
 }
